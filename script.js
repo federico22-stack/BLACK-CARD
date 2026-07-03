d5f97c45ba5726d49299daa36ea63ec8977f5e11
Federico Mercado <federico22merca@gmail.com>
2026-07-03 11:48:09 -0300

    Primer commit del proyecto BLACK CARD

---
+++ script.js
@@ -0,0 +1,47 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbw1yvfzyoZcqzHJCRqs6iGeA7PcfP7Zy9BJsIMbNDLxB2_e0SSGbc2vbjpovBblo3Ln/exec";
+
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code") || "BC001";
+
+const codeEl = document.getElementById("code");
+const statusEl = document.getElementById("status");
+const scanBtn = document.getElementById("scanBtn");
+codeEl.innerText = code;
+
+// estado inicial
+statusEl.innerText = "VERIFICANDO...";
+
+async function checkCode() {
+    try {
+        const res = await fetch(API_URL + "?code=" + code);
+        const data = await res.json();
+
+        if (data.status === "VALID") {
+            statusEl.innerHTML = "🟢 ACCESO PERMITIDO";
+            statusEl.style.color = "#00ff88";
+        } 
+        
+        else if (data.status === "REDEEMED") {
+            statusEl.innerHTML = "🔴 YA UTILIZADO";
+            statusEl.style.color = "#ff3b3b";
+        } 
+        
+        else {
+            statusEl.innerHTML = "❌ INVÁLIDO";
+            statusEl.style.color = "#ff3b3b";
+        }
+
+    } catch (err) {
+        console.log(err);
+        statusEl.innerHTML = "⚠ ERROR DE CONEXIÓN";
+        statusEl.style.color = "orange";
+    }
+}
+
+checkCode();
+
+scanBtn.addEventListener("click", function () {
+
+    alert("Botón conectado correctamente");
+
+});
